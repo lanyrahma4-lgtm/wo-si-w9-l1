54d123c8c98a46876fe839e6e378a6eb72f19bd0
lanyrahma4-lgtm <lanyrahma4@gmail.com>
2026-04-14 15:30:06 +0700

    first

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "uji coba";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_kampus";
 
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!koneksi) {
+  die("koneksi gagal: ". mysqli_connect_error());
+}
+
+echo "coba aja";
 ?>
